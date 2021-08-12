bc142700f00f23f3e6a00b4e71d8f0b729719ecd
John <arunsahu44@gmail.com>
2021-08-12 21:03:06 +0530

    read write file

--- file_ops.cpp
+++ file_ops.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<sys/types.h>
 #include<unistd.h>
+#include<fcntl.h>
 
 using namespace std;
 
@@ -22,8 +23,13 @@ file_ops::~file_ops()
 }
 
 int main(){
+    int size,fd;
+    char buff[10];
+    fd= open("file.txt",O_RDWR|O_CREAT,0777);
     pid_t mypid;
     mypid = getpid();
-    cout<<mypid;
+    size = read(fd,&buff,10);
+
+    cout<<mypid<<endl<<buff<<endl;
     return 0;
 }
